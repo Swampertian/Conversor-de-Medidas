3dc35055d3edf4ecb59d660d0f5fe706cd99e2c6
Pedro Lucas <143046756+Swampertian@users.noreply.github.com>
2024-08-19 19:42:46 -0300

    Add files via upload

--- classes/calculadoraNum.php
+++ classes/calculadoraNum.php
@@ -1,1 +1,46 @@
+<?php
+
+class CalculadoraNum {
+    private $numero1;
+    private $numero2;
+
+    public function setNumeros($a, $b) {
+        $this->numero1 = $a;
+        $this->numero2 = $b;
+    }
+
+    public function somar() {
+        return $this->numero1 + $this->numero2;
+    }
+
+    public function subtrair() {
+        return $this->numero1 - $this->numero2;
+    }
+
+    public function multiplicar() {
+        return $this->numero1 * $this->numero2;
+    }
+
+    public function dividir() {
+        if ($this->numero2 == 0) {
+            return 'Erro: Divisão por zero';
+        }
+        return $this->numero1 / $this->numero2;
+    }
+    public function raizQuadrada() {
+        if ($this->numero1 < 0) {
+            return 'Erro: Número negativo';
+        }
+        return sqrt($this->numero1);
+    }
+    public function logaritmo() {
+        if ($this->numero1 <= 0 || $this->numero2 <= 0) {
+            return 'Erro: Base e logaritmando devem ser maiores que zero';
+        }
+        return log($this->numero2) / log($this->numero1); // Calcula o logaritmo do número 2 na base do número 1
+    }
+
+}
+
+?>
 
